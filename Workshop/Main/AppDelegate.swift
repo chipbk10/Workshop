87a10d8bcb3dff034c2e33d94d113248153975eb
Hieu Luong <dinh.hieu.luong@ing.com>
2022-07-26 14:15:29 +0200

    initiate project

---
+++ Workshop/Main/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Workshop
+//
+//  Created by Hieu Luong on 26/07/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
